f93a184d93fe98ce06a997aa525f169b7d13aee4
Juan Castro <j.castro@tukaiz.com>
2011-12-30 13:14:50 -0600

    Added a site layout

---
+++ spec/controllers/pages_controller_spec.rb
@@ -0,0 +1,60 @@
+require 'spec_helper'
+
+describe PagesController do
+  render_views
+
+  describe "GET 'home'" do
+    it "returns http success" do
+      get 'home'
+      response.should be_success
+    end
+
+    it "should have the right title" do
+      get 'home'
+      response.should have_selector("title", :content => "Ruby on Rails Tutorial Sample App | Home")
+    end
+
+    it "should have a non-blank boyd" do
+      get 'home'
+      response.body.should_not =~ /<body>\s*<\/body>/
+    end
+  end
+
+
+  describe "GET 'contact'" do
+    it "returns http success" do
+      get 'contact'
+      response.should be_success
+    end
+
+    it "should have the right title" do
+      get 'contact'
+      response.should have_selector("title", :content => "Ruby on Rails Tutorial Sample App | Contact")
+    end
+  end
+
+  describe "GET 'about'" do
+    it "returns http success" do
+      get 'about'
+      response.should be_success
+    end
+
+    it "should have the right title" do
+      get 'about'
+      response.should have_selector("title", :content => "Ruby on Rails Tutorial Sample App | About")
+    end
+  end
+
+  describe "GET 'tukaiz'" do
+    it "returns http success" do
+      get 'tukaiz'
+      response.should be_success
+    end
+
+    it "should have the right title" do
+      get 'tukaiz'
+      response.should have_selector("title", :content => "Ruby on Rails Tutorial Sample App | Tukaiz")
+    end
+  end
+
+end
