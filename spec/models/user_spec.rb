40f1b5a870353b23e7419158fc90be8fa8eba6c4
Juan Castro <j.castro@tukaiz.com>
2012-01-03 15:40:11 -0600

    user model validations

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,18 @@
 require 'spec_helper'
 
 describe User do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "should create a new instance given a valid attribute" do
+    User.create!(:name => "Example User", :email => "example@example.com")
+  end
+
+  it "should require name" do
+    no_name_user = User.new(:email => "example@example.com")
+    no_name_user.should_not be_valid
+  end
+
+  it "should require email" do
+    no_email_user = User.new(:name => "Example User")
+    no_email_user.should_not be_valid
+  end
+
 end
