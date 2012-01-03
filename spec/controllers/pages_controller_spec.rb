cbd58d9396453dda96fb273e025cbd25ea66f4c4
Juan Castro <j.castro@tukaiz.com>
2012-01-03 12:23:53 -0600

    Added new helper

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -7,22 +7,22 @@ describe PagesController do
     @base_title = "Ruby on Rails Tutorial Sample App"
   end
 
-  # describe "GET 'home'" do
-  #    it "returns http success" do
-  #      get 'home'
-  #      response.should be_success
-  #    end
-  #
-  #    it "should have the right title" do
-  #      get 'home'
-  #      response.should have_selector("title", :content => "#{@base_title} | Home")
-  #    end
-  #
-  #    it "should have a non-blank boyd" do
-  #      get 'home'
-  #      response.body.should_not =~ /<body>\s*<\/body>/
-  #    end
-  #  end
+  describe "GET 'home'" do
+      it "returns http success" do
+        get 'home'
+        response.should be_success
+      end
+   
+      it "should have the right title" do
+        get 'home'
+        response.should have_selector("title", :content => "#{@base_title} | Home")
+      end
+   
+      it "should have a non-blank boyd" do
+        get 'home'
+        response.body.should_not =~ /<body>\s*<\/body>/
+      end
+    end
 
 
   describe "GET 'contact'" do
