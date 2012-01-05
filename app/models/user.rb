368d00e74cbb5573f303e16f85cb44335f3cd090
Juan Castro <j.castro@tukaiz.com>
2012-01-05 15:21:54 -0600

    Added sign in validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,12 +14,10 @@ class User < ActiveRecord::Base
     encrypted_password == encrypt(submitted_password)
   end
 
-  class << self
-    def self.authenticate(email, submitted_password)
-      user = find_by_email(email)
-      return nil if user.nil?
-      return user if user.has_password?(submitted_password)
-    end
+  def self.authenticate(email, submitted_password)
+    user = find_by_email(email)
+    return nil if user.nil?
+    return user if user.has_password?(submitted_password)
   end
 
   private
