c97d8e93a1ec7ad954d841503116e53eed8231bc
Juan Castro <j.castro@tukaiz.com>
2012-01-06 10:27:18 -0600

    Implemented sign in/sign out functionality

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,7 +17,9 @@ class UsersController < ApplicationController
   def create
     @user = User.new(params[:user])
     if @user.save
-      redirect_to user_path(@user), :flash => { :success => "Welcome to the Sample App!" }
+      sign_in @user
+      flash[:success] = "Welcome to the Sample App!"
+      redirect_to @user
     else
       render 'new'
       @title = "Sign up"
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -2,7 +2,7 @@ module SessionsHelper
 
   def sign_in(user)
     cookies.permanent.signed[:remember_token] = [user.id, user.salt]
-    current_user = user
+    self.current_user = user
   end
 
   def current_user=(user)
@@ -17,10 +17,15 @@ module SessionsHelper
     !current_user.nil?
   end
 
+  def sign_out
+    cookies.delete(:remember_token)
+    self.current_user = nil
+  end
+
   private
 
     def user_from_remember_token
-      User.authenticate_with_salt(*, remember_token)
+      User.authenticate_with_salt(*remember_token)
     end
 
     def remember_token
